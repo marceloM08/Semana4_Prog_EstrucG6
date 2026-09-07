19747d227978035080a5ab6937f7be1e60b33222
marcelo.molina@uamv.edu.ni <marcelo.molina@uamv.edu.ni>
2026-09-07 10:18:55 -0600

    Ejercicio1

---
+++ Ejercicio1.py
@@ -0,0 +1,10 @@
+def calcular_pago(horas, tarifa):
+    pago = horas * tarifa
+    print("Pago dentro de la función: C$", pago)
+
+
+calcular_pago(40, 120)
+
+print(pago)
+# La siguiente instrucción produciría NameError:
+# print(pago)
